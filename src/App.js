582e2d84b0528ffb2ddfa486f0981d3bb58a7cd1
sam <sblan45@gmail.com>
2021-03-22 21:46:51 -0400

    makes D3Chart dynamic and adds real url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import ChartWrapper from './ChartWrapper'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <ChartWrapper />
+          <Navbar bg="light">
+            <Navbar.Brand>Barchart</Navbar.Brand>
+          </Navbar>
+          <Container>
+            <ChartWrapper />
+          </Container>
       </div>
     )
   }
 }
+
+export default App
--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,7 +1,38 @@
 import * as d3 from 'd3'
 
+const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json"
+
+const WIDTH = 800
+const HEIGHT = 500
+
 export default class D3Chart {
+
   constructor(element) {
+    const svg = d3.select(element)
+      .append("svg")
+        .attr("width", WIDTH)
+        .attr("height", HEIGHT)
     
+    d3.json(url).then( data => {
+      const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.height)])
+        .range([0,HEIGHT])
+
+      const x = d3.scaleBand()
+        .domain(data.map(d => d.name))
+        .range([0, WIDTH])
+        .padding(0.5)
+
+      const rects = svg.selectAll('rect')
+        .data(data)
+
+      rects.enter().append('rect')
+        .attr("x", d => x(d.name))
+        .attr("y", d => HEIGHT - y(d.height))
+        .attr("width", x.bandwidth)
+        .attr("height", d => y(d.height))
+        .attr("fill", "grey")
+    })
+      
   }
 }
