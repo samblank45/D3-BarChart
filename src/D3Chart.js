2bba5b08b22f1d2b99c150ad311c4097e7d6c7a4
sam <sblan45@gmail.com>
2021-03-27 22:00:51 -0400

    adds update() in D3Chart and rearranges code. Adds D3 general update pattern

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -8,56 +8,79 @@ const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
 export default class D3Chart {
 
   constructor(element) {
-    const svg = d3.select(element)
+    this.svg = d3.select(element)
       .append("svg")
         .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
         .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
       .append("g")
         .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+
+    this.svg.append("text")
+      .attr("x", WIDTH / 2)
+      .attr("y", HEIGHT + 50)
+      .attr("text-anchor", "middle")
+      .text("the world's tallest men")
+
+    this.svg.append("text")
+      .attr("x", - (HEIGHT / 2))
+      .attr("y", -50)
+      .attr("text-anchor", "middle")
+      .text("Height in cm")
+      .attr("transform", "rotate(-90)") 
+
+    this.xAxisGroup = this.svg.append('g')
+      .attr("transform", `translate(0, ${HEIGHT})`)
+
+    this.yAxisGroup = this.svg.append('g')
+
     
-    d3.json(url).then( data => {
-      const y = d3.scaleLinear()
-        .domain([
-          d3.min(data, d => d.height) * .95, 
-          d3.max(data, d => d.height)])
-        .range([HEIGHT,0])
-
-      const x = d3.scaleBand()
-        .domain(data.map(d => d.name))
-        .range([0, WIDTH])
-        .padding(0.6)
-
-      const xAxisCall = d3.axisBottom(x)
-      svg.append("g")
-        .attr("transform", `translate(0, ${HEIGHT})`)
-        .call(xAxisCall)
-
-      const yAxisCall = d3.axisLeft(y)
-      svg.append("g").call(yAxisCall)
-      
-      svg.append("text")
-          .attr("x", WIDTH / 2)
-          .attr("y", HEIGHT + 50)
-          .attr("text-anchor", "middle")
-          .text("the world's tallest men")
-      
-      svg.append("text")
-          .attr("x", - (HEIGHT / 2))
-          .attr("y", -50)
-          .attr("text-anchor", "middle")
-          .text("Height in cm")
-          .attr("transform", "rotate(-90)")
-
-      const rects = svg.selectAll('rect')
-        .data(data)
-
-      rects.enter().append('rect')
-        .attr("x", d => x(d.name))
-        .attr("y", d => y(d.height))
-        .attr("width", x.bandwidth)
-        .attr("height", d => HEIGHT - y(d.height))
-        .attr("fill", "grey")
+    d3.json(url).then(data => {
+      this.data = data
+      d3.interval( () => {
+        this.update()
+      }, 1000)  
     })
       
+  }
+  update() {
+    const y = d3.scaleLinear()
+      .domain([
+        d3.min(this.data, d => d.height) * .95, 
+        d3.max(this.data, d => d.height)])
+      .range([HEIGHT,0])
+
+    const x = d3.scaleBand()
+      .domain(this.data.map(d => d.name))
+      .range([0, WIDTH])
+      .padding(0.6)
+
+    const xAxisCall = d3.axisBottom(x)
+    this.xAxisGroup.call(xAxisCall)
+
+    const yAxisCall = d3.axisLeft(y)
+    this.yAxisGroup.call(yAxisCall)
+
+    //D3 GENERAL UPDATE PATTERN
+    // DATA JOIN
+    const rects = this.svg.selectAll('rect')
+      .data(this.data)
+
+    //EXIT
+    rects.exit().remove()
+
+    //UPDATE
+    rects.attr("class","update")
+      .attr("x", d => x(d.name))
+      .attr("y", d => y(d.height))
+      .attr("width", x.bandwidth)
+      .attr("height", d => HEIGHT - y(d.height))
+
+    // ENTER
+    rects.enter().append('rect')
+      .attr("x", d => x(d.name))
+      .attr("y", d => y(d.height))
+      .attr("width", x.bandwidth)
+      .attr("height", d => HEIGHT - y(d.height))
+      .attr("fill", "grey")
   }
 }
